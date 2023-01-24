739cfd01491a0299fd9cf21e587b78e06432e442
miyashy <69844491+miyashy@users.noreply.github.com>
2023-01-25 00:29:45 +0900

    ユーザーのAPIを追加(仮実装)

--- main.go
+++ main.go
@@ -10,5 +10,16 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
 	})
+	e.GET("/users/:userId", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, User{Id: "dummy", Name: "Name"})
+	})
+	e.POST("/users/", func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, User{Id: "dummy", Name: "Name"})
+	})
 	e.Logger.Fatal(e.Start(":1234"))
+}
+
+type User struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }

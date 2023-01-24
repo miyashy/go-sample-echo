017af35062d12cc77d712b14452a0aacf9ba3357
miyashy <69844491+miyashy@users.noreply.github.com>
2023-01-25 00:37:16 +0900

    APIの仮実装

--- main.go
+++ main.go
@@ -16,10 +16,44 @@ func main() {
 	e.POST("/users/", func(c echo.Context) error {
 		return c.JSON(http.StatusCreated, User{Id: "dummy", Name: "Name"})
 	})
+	e.GET("/todo/:todoId", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, Todo{
+			Id:          "dummy",
+			UserId:      "dummyUserID",
+			Status:      "TODO",
+			Description: "DummyTodo",
+		})
+	})
+	e.PATCH("/todo/:todoId", func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, Todo{
+			Id:          "dummy",
+			UserId:      "dummyUserID",
+			Status:      "TODO",
+			Description: "DummyTodo",
+		})
+	})
+	e.DELETE("/todo/:todoId", func(c echo.Context) error {
+		return c.NoContent(http.StatusNoContent)
+	})
+	e.POST("/todo", func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, Todo{
+			Id:          "dummy",
+			UserId:      "dummyUserID",
+			Status:      "TODO",
+			Description: "DummyTodo",
+		})
+	})
 	e.Logger.Fatal(e.Start(":1234"))
 }
 
 type User struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
+}
+
+type Todo struct {
+	Id          string `json:"id"`
+	UserId      string `json:"userId"`
+	Status      string `json:"status"`
+	Description string `json:"description"`
 }
